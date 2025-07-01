47e0ca24393bc10b8f7838c30a56fd6f957e7752
rakshitDarji007 <rakshit.darji007@gmail.com>
2025-07-01 15:33:55 -0500

    initial setup

---
+++ diet-planner/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Box p={4}>
+      <Heading as='h1' size="lg" color="blue.600">
+        Diet Planner App
+      </Heading>
+    </Box>
+  );
+}
+
+export default App;
